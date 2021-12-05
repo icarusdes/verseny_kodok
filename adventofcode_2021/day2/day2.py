916e2e00b9a827eb6af8b6356a3a56ba2505ba03
Csaba Mátyás-Peti <matyaspcsaba@gmail.com>
2021-12-05 17:24:03 +0100

    Day2 code

---
+++ adventofcode_2021/day2/day2.py
@@ -0,0 +1,22 @@
+import sys
+
+input_file = open(sys.argv[1], 'r')
+lines = input_file.read().splitlines()  
+
+x, y = 0, 0
+
+for line in lines: 
+    [command, unit] = line.split() 
+
+    if command == "forward": 
+        x += int(unit)
+    elif command == "down": 
+        y += int(unit)
+    elif command == "up": 
+        y -= int(unit)
+        if (y < 0): 
+            print("He?")
+    else: 
+        print ("Bay van")
+
+print( x* y)
---
+++ adventofcode_2021/day2/day2_v2.py
@@ -0,0 +1,23 @@
+import sys
+
+input_file = open(sys.argv[1], 'r')
+lines = input_file.read().splitlines()  
+
+x, y, aim = 0, 0, 0
+
+for line in lines: 
+    [command, unit] = line.split() 
+    unit = int(unit)
+    if command == "forward": 
+        x += unit
+        y += aim * unit
+    elif command == "down": 
+        aim += int(unit)
+    elif command == "up": 
+        aim -= int(unit)
+        if (y < 0): 
+            print("He?")
+    else: 
+        print ("Bay van")
+
+print( x* y)
